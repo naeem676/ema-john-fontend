81aa098e555a01ab8038a7ee6a45e709d915484c
naeem uddin <ali.naeem939@gmail.com>
2021-03-28 16:19:21 +0600

    ema-john-fontent

---
+++ src/components/Inventory/Inventory.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+
+
+const Inventory = () => {
+    
+   
+    const handleAddProducts = () => {
+        const products = {}
+       fetch('http://localhost:5000/addProducts', {
+           method: 'POST',
+           headers:{'Content-Type': 'application/json'},
+           body: JSON.stringify(products)
+       })
+    }
+    return (
+        <div>
+        <form action={handleAddProducts}>
+            <p><span>Name:</span><input type="text"/></p>
+            <p><span>Price:</span><input type="text"/></p>
+            <p><span>Quantity:</span><input type="text"/></p>
+            <p><span>Picture:</span><input type="file"/></p>
+            <button type='submit'  >add products</button>
+            
+        </form>
+        
+            
+        </div>
+    );
+};
+
+export default Inventory;
---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDZVCYYOdhtplTa3JFUSTylLrVOzg9qahY",
+  authDomain: "ema-john-2021.firebaseapp.com",
+  projectId: "ema-john-2021",
+  storageBucket: "ema-john-2021.appspot.com",
+  messagingSenderId: "229783258274",
+  appId: "1:229783258274:web:4d54608d671e4b59aa43fb"
+};
+export default firebaseConfig;
