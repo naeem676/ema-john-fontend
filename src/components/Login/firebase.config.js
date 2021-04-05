4d3886a4f281baa286412d4ab754c3c0a64cce73
naeem uddin <ali.naeem939@gmail.com>
2021-04-05 20:57:35 +0600

    just setting dotenv

--- src/components/Login/firebase.config.js
+++ src/components/Login/firebase.config.js
@@ -6,15 +6,8 @@ const firebaseConfig = {
   storageBucket: `${env.STORAGE_BUCKET}`,
   messagingSenderId: `${env.MESSAGING_SENDER_ID}`,
   appId: `${env.APP_ID}`
-};;
-  export default firebaseConfig;
-
-  // apiKey: "AIzaSyDZVCYYOdhtplTa3JFUSTylLrVOzg9qahY",
-  // authDomain: "ema-john-2021.firebaseapp.com",
-  // projectId: "ema-john-2021",
-  // storageBucket: "ema-john-2021.appspot.com",
-  // messagingSenderId: "229783258274",
-  // appId: "1:229783258274:web:4d54608d671e4b59aa43fb"
+};
+export default firebaseConfig;
 
 
 
